78d0e5bc9685761ba7953efc9c4ef4ed1d8cf254
Diego Arturo Parra Molina <48141200+DiegoArturoParra@users.noreply.github.com>
2020-02-26 00:38:32 -0500

    se instancio un objeto.
    se instancio un objeto para correr el proyecto.

--- src/main/java/edu/cundi/poligonos/Controllers/Main.java
+++ src/main/java/edu/cundi/poligonos/Controllers/Main.java
@@ -1,12 +1,14 @@
 
 package edu.cundi.poligonos.Controllers;
 
+import edu.cundi.poligonos.Views.Formulario;
+
 /**
  *
  * @author diego parra
  */
 public class Main {
     public static void main(String[] args) {
-        
+        Formulario ventana = new Formulario();
     }
 }
